3d79853d4db97d270b6a71a0b4c4e3495628b4af
Lukas Svaty <svatylukas@gmail.com>
2013-12-26 13:13:23 +0100

    Added implementation of 3D nurbs volume interpolation

---
+++ cf3/mesh/BlockMesh/NurbsEvaluation.cpp
@@ -0,0 +1,154 @@
+// Copyright (C) 2010-2013 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include "common/Builder.hpp"
+#include "common/Core.hpp"
+#include "common/Exception.hpp"
+#include "common/EventHandler.hpp"
+#include "common/Log.hpp"
+#include "common/OptionComponent.hpp"
+#include "common/OptionList.hpp"
+#include "common/OptionT.hpp"
+#include "common/Table.hpp"
+#include "common/XML/SignalFrame.hpp"
+#include "common/XML/SignalOptions.hpp"
+#include "common/Timer.hpp"
+
+#include "common/PE/Comm.hpp"
+#include "mesh/BlockMesh/NurbsEvaluation.hpp"
+#include <vector>
+
+namespace cf3 {
+namespace mesh {
+namespace BlockMesh {
+
+
+//ComponentBuilder < NurbsEvaluation, Component, LibBlockMesh > NurbsEvaluation_Builder;
+
+NurbsEvaluation::NurbsEvaluation(const std::string& name) : Component(name)
+{
+}
+
+NurbsEvaluation::~NurbsEvaluation()
+{
+}
+
+
+
+//------------------------------------------------------------	CoxDeBoor()
+//
+Real NurbsEvaluation::CoxDeBoor(std::vector<Real> param, unsigned int i, int dimension, std::vector<unsigned int>order) {
+	if(order[dimension]==1)
+	{
+		if( Knots[dimension][i] <= param[dimension] && param[dimension] <= Knots[dimension][i+1] ) {
+			return 1.0f;
+		}
+		return 0.0f;
+	}
+	Real Den1 = Knots[dimension][i+order[dimension]-1] - Knots[dimension][i];
+	Real Den2 = Knots[dimension][i+order[dimension]] - Knots[dimension][i+1];
+	Real Eq1=0,Eq2=0;
+    std::vector<unsigned int> new_order;
+    for (int k=0; k<=dimension; k++)
+        new_order.push_back(order[dimension]-1);
+	if(Den1>0) {
+		Eq1 = (param[dimension]-Knots[dimension][i]) / Den1 * CoxDeBoor(param,i,dimension,new_order);
+	}
+	if(Den2>0) {
+		Eq2 = (Knots[dimension][i+order[dimension]]-param[dimension]) / Den2 * CoxDeBoor(param,i+1,dimension,new_order);
+	}
+	return Eq1+Eq2;
+}
+
+Real NurbsEvaluation::BSplineBasis(std::vector<Real> param, std::vector<unsigned int> index) {
+    Real Den = 0;
+    for (unsigned int i=0; i < num_points[0]; i++)
+        for (unsigned int j=0; j < num_points[1]; j++)
+            for (unsigned int k=0; k < num_points[2]; k++) 
+                Den += (Weights[k][j][i] *  
+                    CoxDeBoor(param, i, 0, order) * 
+                    CoxDeBoor(param, j, 1, order) * 
+                    CoxDeBoor(param, k, 2, order)); 
+    if (Den == 0)
+        return 0.0f;
+    return (Weights[index[2]][index[1]][index[0]] * 
+        CoxDeBoor(param, index[0], 0, order) * 
+        CoxDeBoor(param, index[1], 1, order) * 
+        CoxDeBoor(param, index[2], 2, order) / Den);
+}
+
+void NurbsEvaluation::GetOutpoint(std::vector<Real> param, Real OutPoint[]){
+
+	// sum the effect of all CV's on the curve at this point to 
+	// get the evaluated curve point
+	// 
+	for(unsigned int i=0; i < num_points[0]; i++) {
+        for (unsigned int j=0; j < num_points[1]; j++) {
+            for (unsigned int k=0; k < num_points[2]; k++) {
+                std::vector<unsigned int> index;
+                index.push_back(i);
+                index.push_back(j);
+                index.push_back(k);
+                // calculate the effect of this point on the curve
+                Real Val = BSplineBasis(param,index);
+
+                // sum effect of CV on this part of the curve
+                OutPoint[0] += Val * Points[k][j][i][0];
+                OutPoint[1] += Val * Points[k][j][i][1];
+                OutPoint[2] += Val * Points[k][j][i][2];
+            }
+        }
+	}
+}
+
+void NurbsEvaluation::AddPoint(cf3::common::Table<Real>::ConstRow  point, const Real weight, const int x, const int y, const int z) {
+    if (Points.size() <= x) {
+		Points.resize(x+1);
+		Weights.resize(x+1);
+	}
+    if (Points[x].size() <= y) {
+		Points[x].resize(y+1);
+		Weights[x].resize(y+1);
+	}
+    if (Points[y].size() <= z) {
+		Points[y].resize(z+1);
+		Weights[y].resize(z+1);
+	}
+	std::vector<Real> p;
+	p.push_back(point[0]);
+	p.push_back(point[1]);
+	p.push_back(((point.size()==2)?0:point[2]));
+    Points[x][y][z] = p;
+    Weights[x][y][z] = weight;
+}
+
+void NurbsEvaluation::AddKnots(std::vector<Real> k) {
+    Knots.push_back(k);
+}
+void NurbsEvaluation::InitNutbs(){
+    
+	num_points.push_back(Points[0][0].size());
+    num_points.push_back(Points[0].size());
+    num_points.push_back(Points.size());
+    
+    //degree +1; m = p + n + 1 where n is the number of control points and p 
+    //is the degree of the desired blending function m is length of knot std::vector.
+    order.push_back(Knots[0].size() - num_points[0]); 
+    order.push_back(Knots[1].size() - num_points[1]);
+    order.push_back(Knots[2].size() - num_points[2]);
+}
+
+void NurbsEvaluation::SetDimension(int dimension) {
+    if (dimension == 2) 
+        dim = 2;
+    else 
+        dim =3;
+}
+
+
+} // BlockMesh
+} // mesh
+} // cf3
---
+++ cf3/mesh/BlockMesh/NurbsEvaluation.hpp
@@ -0,0 +1,80 @@
+// Copyright (C) 2010-2013 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#ifndef cf3_mesh_BlockMesh_NurbsEvaluation_hpp
+#define cf3_mesh_BlockMesh_NurbsEvaluation_hpp
+
+#include "common/CF.hpp"
+#include "common/Component.hpp"
+#include <vector>
+
+namespace cf3 {
+namespace mesh {
+namespace BlockMesh {
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+class NurbsEvaluation : public common::Component
+{
+public:
+
+  NurbsEvaluation(const std::string& name);
+  ~NurbsEvaluation();
+
+  static std::string type_name () { return "NurbsEvalution"; }
+  
+  /// compute the values of each coordinate
+  /// @param param - vector of parameters u, v, w
+  /// @param OutPoint - coordinetes of the result point
+  void GetOutpoint(std::vector<Real> param, Real OutPoint[]);
+  /// Add control points in 4D vector
+  /// @param Points 4D vector of matrixes
+  void AddPoint(cf3::common::Table<Real>::ConstRow  point, const Real weight, const int x, const int y, const int z);
+  /// Add weights for each control points (default 1)
+  /// @param Weights - 3D matrix of weights
+  void AddWeights(std::vector<std::vector<std::vector<Real> > >Weights);
+  /// Add knot vectors for each direction
+  /// @param Knots - vector of Kont vectors
+  void AddKnots(std::vector<Real>Knots);
+  /// Initialise the order vector and num_points vector for ocmputation
+  void InitNutbs();
+  /// Set weather to work in 2D/3D space
+  /// @param dimension 2 or 3 (depends on dimension of computation)
+  void SetDimension(int dimension);
+  
+private:
+  std::vector<unsigned int>order;
+  std::vector<std::vector<Real> >Knots;
+  std::vector<std::vector<std::vector<std::vector<Real> > > >Points;
+  std::vector<std::vector<std::vector<Real> > >Weights;
+  std::vector<unsigned int> num_points;
+  int dim;
+  
+  /// Compute the basis function for NURBS by Cox De-boor algorithm
+  /// @param param - vector of parameters u, v, w
+  /// @param i - index of computed bases
+  /// @param dimension - direction of table block curve(0, 1, 2) 
+  /// @param order - orders of the curve in each direction
+  /// @param Knots - knot vectors for the curve in each direction
+  Real CoxDeBoor(std::vector<Real> param, unsigned int i, int dimension, std::vector<unsigned int>order);
+
+  /// Compute the value of the point with cox de-boor algorithms and weights of points
+  /// @param param - vector of parameters u, v, w
+  /// @param index - index of computed bases
+  Real BSplineBasis(std::vector<Real> param, std::vector<unsigned int> index);
+ 
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // BlockMesh
+} // mesh
+} // cf3
+
+#endif /* cf3_mesh_BlockMesh_NurbsEvaluation_hpp */
+
